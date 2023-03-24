96ad85eee2ed5203c47ba1d3be96beba4d9cba88
Omkar Kawade <128675006+Omkarkawade@users.noreply.github.com>
2023-03-24 10:40:08 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+/* 
+Write a proogram to find if the number is odd or even
+*/
+
+class EvenOdd
+{
+	
+	public static void main(String args[]){
+
+	int a=10;
+	if(a%2==0)
+	{
+		System.out.println("The Number is Even");
+	}
+	else
+	{
+		System.out.println("The Number is Odd");
+	}
+	}
+}
